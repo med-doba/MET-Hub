a5e8a566678ac225b0e726e0f46c511fd8f2ff51
Mohammed Ed-dobaji <med-doba@e2r1p5.1337.ma>
2024-02-22 09:43:19 +0100

    add some content to the home page

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Zs3N6HzfbGPzUuOmZxUGbqiItiExVDWpiLIqWfaFHoo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9YN5Qil/6NSbzpUv6mFjjqMxibVIW/XhxVetYqc3TVA=\"\n}"
